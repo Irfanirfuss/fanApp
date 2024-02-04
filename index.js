3b5eba3de3d42bac8baeb5ce8fbf8fd58fe16b15
Irfan <irfanirfuss2000@gmail.com>
2024-02-04 11:24:08 +0530

    Fan

---
+++ index.js
@@ -0,0 +1,22 @@
+let main = document.getElementById("main");
+let img = document.getElementById("img");
+
+function onFan() {
+    img.style.animationDuration = 4 + "s";
+}
+
+function offFan() {
+    img.style.animationDuration = 0 + "s";
+}
+
+function one() {
+    img.style.animationDuration = 3 + "s";
+}
+
+function two() {
+    img.style.animationDuration = 2 + "s";
+}
+
+function three() {
+    img.style.animationDuration = 1 + "s";
+}
